7bc5e1acf01d81e2dc2f60b770e3450e71d592e7
zhang <zhangwei_w8284@proware.net>
2016-12-20 19:38:54 -0800

    finish first step

--- database/migrations/2016_12_19_084455_create_article_table.php
+++ database/migrations/2016_12_19_084455_create_article_table.php
@@ -12,7 +12,14 @@ class CreateArticleTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('articles', function(Blueprint $table)
+    {
+        $table->increments('id');
+        $table->string('title');
+        $table->text('body')->nullable();
+        $table->integer('user_id');
+        $table->timestamps();
+    });
     }
 
     /**
